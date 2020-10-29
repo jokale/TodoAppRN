473c426fea037c78a9166558b3553a651a455be6
jokale <kalemajoanna@gmail.com>
2020-10-29 12:28:59 +0000

    create flatlist

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [todos, setTodos] = useState([
+    {text: 'Wash my hair', key: '1'},
+    {text: 'Moisturise and blow dry my hair', key: '2'},
+    {text: 'Moisturise and cornrow ', key: '3'},
+    {text: 'Go and get food shop', key: '4'},
+    {text: 'Wash car', key: '5'},
+
+  ])
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* header */ }
+      <View style={styles.content}>
+        {/* to do form */}
+        <View style={styles.list}>
+          <FlatList/>
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,8 +28,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'pink',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
