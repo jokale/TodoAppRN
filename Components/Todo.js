7559936caa0188145101d160472a656a77778a44
jokale <kalemajoanna@gmail.com>
2020-11-01 19:05:23 +0000

    add icon and more flexbox

--- Components/Todo.js
+++ Components/Todo.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity} from 'react-native';
-
+import { StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons'; 
 // placing item as a prop to ensure that i can have access to it 
 //when i import the todo prop
 export default function Todo({item, pressHandler}) {
 
     return (
         <TouchableOpacity onPress={() => pressHandler(item.key)}>
-            <Text style={styles.item}>{item.text}</Text>
+            <View style={styles.item}>
+                <MaterialIcons name= 'delete' size={25} color='#FFF5C2'/>
+                <Text style={styles.itemText}>{item.text}</Text>
+            </View>
         </TouchableOpacity>
     )
 }
@@ -21,8 +24,12 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderStyle: "dashed",
         borderRadius: 10,
+        flexDirection: 'row'
         
     },
+    itemText: {
+        marginLeft: 10,
+    }
   
 
 });
